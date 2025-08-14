f3f919a986bc34943cbf4e59faabb139766b4c1e
Deepak Pathik <deepakpathik2005@gmail.com>
2025-08-15 02:13:55 +0530

    Time: 34 ms (39.27%), Space: 45.2 MB (37.67%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,34 +1,38 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        int[] ans = new int[]{0, 0};
-        
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
-        }
-        
-        for (int i = 0; i < n - 1; i++) {
-            if (s.charAt(i) == s.charAt(i + 1)) {
-                dp[i][i + 1] = true;
-                ans[0] = i;
-                ans[1] = i + 1;
+        String longest = "";
+         //s = "abba";
+        s = s.replace("","#");
+        for (int center = 0; center < s.length(); center++) {
+//            String longestPair = getLongest(s, center, true);
+//            if (longestPair.length() > longest.length()) {
+//                longest = longestPair;
+//            }
+            String longestOdd = getLongest(s, center, false);
+            if (longestOdd.length() > longest.length()) {
+                longest = longestOdd;
             }
         }
-        
-        for (int diff = 2; diff < n; diff++) {
-            for (int i = 0; i < n - diff; i++) {
-                int j = i + diff;
-                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
-                    dp[i][j] = true;
-                    ans[0] = i;
-                    ans[1] = j;
-                }
-            }
-        }
-        
-        int i = ans[0];
-        int j = ans[1];
-        return s.substring(i, j + 1);
+        return longest.replace("#","");
     }
+
+    private static String getLongest(String s, int center, boolean czyParzysty) {
+        int ramie = 0;
+        int centerLeft = center;
+        int centerRight = centerLeft;
+        if (czyParzysty) {
+            centerRight++;
+        }
+        while (centerRight + ramie < s.length() && centerLeft - ramie >= 0) {
+            char leftValue = s.charAt(centerLeft - ramie);
+            char rightValue = s.charAt(centerRight + ramie);
+            if (leftValue != rightValue) {
+                break;
+            }
+            ramie++;
+        }
+        return s.substring(centerLeft - ramie + 1, centerRight + ramie );
+    }
+
+
 }
