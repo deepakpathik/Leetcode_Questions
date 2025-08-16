01f392dc317bf5d8b66f838e077a098c2e2c0496
Deepak Pathik <deepakpathik2005@gmail.com>
2025-08-16 19:42:22 +0530

    Time: 4 ms (100%), Space: 44.4 MB (17.15%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,23 +1,13 @@
 class Solution {
-    public boolean check(ArrayList<Integer> list){
-        int low = 0;
-        int high = list.size()-1;
-        while(low<high){
-            if(list.get(low) != list.get(high)){
-                return false;
-            }
-            low++;
-            high--;
-        }
-        return true;
-    }
     public boolean isPalindrome(int x) {
-        if(x<0) return false;
-        ArrayList<Integer> list = new ArrayList<>();
-        while(x!=0){
-            list.add(x%10);
-            x = x/10;
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
         }
-        return check(list);
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
     }
 }
