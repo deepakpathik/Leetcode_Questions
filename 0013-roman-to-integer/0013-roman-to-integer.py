1d11377e70cf8861d15e10f9391d2814a2f5330f
Deepak Pathik <deepakpathik2005@gmail.com>
2025-08-17 22:03:38 +0530

    Time: 23 ms (7.2%), Space: 12.4 MB (53.08%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,61 +1,28 @@
-class Solution:
+class Solution(object):
     def romanToInt(self, s):
-        ans = 0
-        index = 0
-        while index < len(s):
-            c = s[index]
-            if c == 'I':
-                if index + 1 < len(s):
-                    if s[index + 1] == 'V':
-                        ans += 4
-                        index += 2
-                    elif s[index + 1] == 'X':
-                        ans += 9
-                        index += 2
-                    else:
-                        ans += 1
-                        index += 1
-                else:
-                    ans += 1
-                    index += 1
-            elif c == 'V':
-                ans += 5
-                index += 1
-            elif c == 'X':
-                if index + 1 < len(s):
-                    if s[index + 1] == 'L':
-                        ans += 40
-                        index += 2
-                    elif s[index + 1] == 'C':
-                        ans += 90
-                        index += 2
-                    else:
-                        ans += 10
-                        index += 1
-                else:
-                    ans += 10
-                    index += 1
-            elif c == 'L':
-                ans += 50
-                index += 1
-            elif c == 'C':
-                if index + 1 < len(s):
-                    if s[index + 1] == 'D':
-                        ans += 400
-                        index += 2
-                    elif s[index + 1] == 'M':
-                        ans += 900
-                        index += 2
-                    else:
-                        ans += 100
-                        index += 1
-                else:
-                    ans += 100
-                    index += 1
-            elif c == 'D':
-                ans += 500
-                index += 1
-            elif c == 'M':
-                ans += 1000
-                index += 1
-        return ans
+        """
+        :type s: str
+        :rtype: int
+        """
+        val = 0
+        symbs = {'I':1, "V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        for i in range(len(s)-1):
+            if symbs[s[i]] < symbs[s[i+1]]:
+                val -= symbs[s[i]]
+            else:
+                val += symbs[s[i]]
+
+        return val + symbs[s[-1]]
+
+        
+        # symbs = {'I':1, "V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        # val = 0
+
+        # for curr, nxt in zip(s, s[1:]):
+        #     if symbs[curr] < symbs[nxt]:
+        #         val -= symbs[curr]
+        #     else:
+        #         val += symbs[curr]
+
+        # val += symbs[s[-1]]
+        # return val
