b7bde275f7458cdde35154e9bb5602a15b320395
Deepak Pathik <deepakpathik2005@gmail.com>
2025-09-20 00:07:16 +0530

    Time: 35 ms (18.68%), Space: 44.8 MB (73.66%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean equals(int[] freq, int[] currFreq){
+        for(int i = 0;i<freq.length;i++){
+            if(freq[i]>currFreq[i]) return false;
+        }
+        return true;
+    }
+    public String minWindow(String s, String t) {
+        if(t.length()>s.length()) return "";
+        int[] freqT = new int[128];
+        for(int i = 0;i<t.length();i++){
+            freqT[(int) t.charAt(i)]++;
+        }
+        int[] freqCurrentWindow = new int[128];
+        int currentStart = 0;
+        int currentEnd   = Integer.MAX_VALUE;
+        int start = 0;
+        int end   = 0;
+        freqCurrentWindow[s.charAt(start)]++;
+        while(end<s.length()){
+            if(equals(freqT,freqCurrentWindow)){
+                if(currentEnd - currentStart>end-start){
+                    currentEnd = end;
+                    currentStart = start;
+                }
+                freqCurrentWindow[(int) s.charAt(start)]--;
+                start++;               
+            }else if(end<s.length()-1){
+                end++;
+                freqCurrentWindow[(int) s.charAt(end)]++;
+            }else break;
+        }
+        if(currentEnd == Integer.MAX_VALUE ) return "";
+        return s.substring(currentStart,currentEnd+1);
+
+    }
+}
