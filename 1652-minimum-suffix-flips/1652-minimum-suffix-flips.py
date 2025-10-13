014d9f4eb3eb6c3fb1e78b7c31b0ae26c6d177f6
Deepak Pathik <deepakpathik2005@gmail.com>
2025-10-14 01:43:04 +0530

    Time: 59 ms (6.01%), Space: 18.2 MB (11.75%) - LeetHub

--- 1652-minimum-suffix-flips/1652-minimum-suffix-flips.py
+++ 1652-minimum-suffix-flips/1652-minimum-suffix-flips.py
@@ -1,11 +1,25 @@
 class Solution:
-    # def flip(self,s, index):
+    # def minFlips(self,s, index):
     #     for i in range(index,len(s)):
     #         if(s[i] == 0):
     #             s[i] = 1
     #         else:
     #             s[i] = 0
     #     return s
+    # def minFlips(self, target: str) -> int:
+    #     ans = 0
+    #     n = len(target)
+    #     # s = [0]*n
+    #     bit = 0
+    #     for i in range(n):
+    #         if(int(target[i]) != bit):
+    #             ans+=1
+    #             # bit = (bit +1)%2
+    #             if(bit == 0):
+    #                 bit = 1
+    #             else:
+    #                 bit = 0
+    #     return ans
     def minFlips(self, target: str) -> int:
         ans = 0
         n = len(target)
@@ -14,9 +28,5 @@ class Solution:
         for i in range(n):
             if(int(target[i]) != bit):
                 ans+=1
-                # bit = (bit +1)%2
-                if(bit == 0):
-                    bit = 1
-                else:
-                    bit = 0
+                bit = (bit +1)%2
         return ans
